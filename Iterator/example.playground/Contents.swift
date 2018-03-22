d897da11596b23a9bb7fd096839de2e0c52341c5
jimmythai <ayamam1127@gmail.com>
2018-03-22 10:01:38 +0900

    Create Iterator pattern

---
+++ Iterator/example.playground/Contents.swift
@@ -0,0 +1,45 @@
+// Define
+struct Backpack {
+    let name: String
+    let brandName: String
+    let price: Int
+    let currency: String
+}
+
+struct Backpacks {
+    let backpacks: [Backpack]
+}
+
+struct BackpacksIterator: IteratorProtocol {
+    typealias Element = Backpack
+
+    private let backpacks: [Element]
+    private var offset = 0
+
+    init(backpacks: [Element]) {
+        self.backpacks = backpacks
+    }
+
+    mutating func next() -> BackpacksIterator.Element? {
+        defer {
+            offset += 1
+        }
+
+        return backpacks.count > offset ? backpacks[offset] : nil
+    }
+}
+
+extension Backpacks: Sequence {
+    typealias Iterator = BackpacksIterator
+    
+    func makeIterator() -> Iterator {
+        return BackpacksIterator(backpacks: backpacks)
+    }
+}
+
+
+// Use
+let backpacks = Backpacks(backpacks: [Backpack(name: "QUANTUM 70 + 10", brandName: "Deuter", price: 255, currency: "€")])
+for backpack in backpacks {
+    print("Name: \(backpack.name), Brand name: \(backpack.brandName), Price: \(backpack.price)\(backpack.currency)")
+}
