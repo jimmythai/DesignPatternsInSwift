d8b877a5703c3f6b9fde8037bea098b32c5bc522
jimmythai <ayamam1127@gmail.com>
2018-04-06 21:19:43 +0900

    Create Decorator pattern

---
+++ Decorator/example.playground/Contents.swift
@@ -0,0 +1,71 @@
+// Core Component
+protocol iPhone {
+    var cost: Double { get }
+    var name: String { get }
+}
+
+// Concrete Component
+struct iPhone6: iPhone {
+    var cost: Double {
+         return 100
+    }
+    var name: String {
+        return "iPhone 6"
+    }
+}
+
+// Decorator
+protocol iPhoneDecorator: iPhone {
+    var base: iPhone { get }
+}
+
+// Concrete Decorator
+class iPhone6Plus: iPhone {
+    var cost: Double {
+        return 120
+    }
+    var name: String {
+        return "iPhone 6 Plus"
+    }
+}
+
+struct UpgradeStorage: iPhoneDecorator {
+    let base: iPhone
+    
+    var cost: Double {
+        return base.cost + 20
+    }
+    var name: String {
+        return "\(base.name) with bigger storage"
+    }
+}
+
+struct BlackColor: iPhoneDecorator {
+    let base: iPhone
+    
+    var cost: Double {
+        return base.cost
+    }
+    var name: String {
+        return "Black \(base.name)"
+    }
+}
+
+struct GoldColor: iPhoneDecorator {
+    let base: iPhone
+    
+    var cost: Double {
+        return base.cost
+    }
+    var name: String {
+        return "Gold \(base.name)"
+    }
+}
+
+let iphone6 = iPhone6()
+let biggeriPhone = UpgradeStorage(base: iphone6)
+let goldColoriPhone = GoldColor(base: biggeriPhone)
+
+print(goldColoriPhone.name)
+
+
