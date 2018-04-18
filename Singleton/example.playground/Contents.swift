6c48737892f02ee4cec9d7c650bf5e9544d8355a
jimmythai <ayamam1127@gmail.com>
2018-04-18 15:43:55 +0900

    Modify Singleton pattern

--- Singleton/example.playground/Contents.swift
+++ Singleton/example.playground/Contents.swift
@@ -1,9 +1,15 @@
+// Category: Creational
+// When to use: In case you want to guarantee that only one instance of a object is instantiated and have global access to it.
+
 // Define
 class BackpackerManager {
     static let shared = BackpackerManager()
+    
+    var name = "Michael"
     
     private init() {}
 }
 
 // Use
 let backpackerManger = BackpackerManager.shared
+backpackerManger.name
